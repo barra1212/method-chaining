b3bc5874fda835028a4581d58c5ec8ac9f482100
barrycunningham <cunnbarry@gmail.com>
2018-11-10 12:47:37 +0000

    challenge completed

--- js/script.js
+++ js/script.js
@@ -2,5 +2,12 @@ $(document).ready(function() {
  //
  // add your jQuery code here
 
+$("#button1").on("mouseenter", function(){
+ $("#button1").removeClass("makeRed").addClass("makeBorder");
+});
+
+$("#button1").on("mouseleave", function(){
+ $("#button1").addClass("makeRed").removeClass("makeBorder");
+});
 
 }); 
