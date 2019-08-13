f53ce0f63d21c76ce3cfb638ccaeabde7ba4b8f9
barrycunningham <cunnbarry@gmail.com>
2019-08-13 08:17:26 +0000

    Tidy Up

--- js/script.js
+++ js/script.js
@@ -2,12 +2,40 @@ $(document).ready(function() {
  //
  // add your jQuery code here
 
-$("#button1").on("mouseenter", function(){
- $("#button1").removeClass("makeRed").addClass("makeBorder");
-});
+// $("#button1").on("mouseenter", function(){
+//  $("#button1").removeClass("makeRed").addClass("makeBorder");
+// });
 
-$("#button1").on("mouseleave", function(){
- $("#button1").addClass("makeRed").removeClass("makeBorder");
+// $("#button1").on("mouseleave", function(){
+//  $("#button1").addClass("makeRed").removeClass("makeBorder");
+// });
+
+// $("#button1").on("click", function(){
+//  $("#para1").slideToggle("slow");
+// });
+
+// $("#button1").on("click", function(){
+// $("#para1").slideUp(1000);
+// $("#para1").slideDown(1000);
+// });
+
+// $("#button1").on("click", function(){
+// $("#para1").fadeIn(1000);
+
+// $("#button1").on("click", function(){
+// $("#para1").fadeOut(1000);
+// });
+
+// $("#button2").on("click", function(){
+// $("#para2").hide(1000);
+// });
+
+// $("#button2").on("click", function(){
+// $("#para2").show(1000);
+// });
+
+$("#button1").on("click", function(){
+$("p").fadeIn().fadeOut();
 });
 
 }); 
